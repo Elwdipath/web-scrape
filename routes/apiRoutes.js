b8b2fd3364e091f5cf68dfa88f49357f52991da1
Jordan Kaiser <jordan.kaiser@gmail.com>
2020-04-27 15:51:08 -0400

    fixed scraping

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,43 +1,26 @@
 var db = require("../models");
 var axios = require("axios");
-var cheerio = require("cheerio")
+var cheerio = require("cheerio");
 
 module.exports = function (app) {
   app.get("/scrape", function (req, res) {
-    console.log("here")
     // First, we grab the body of the html with axios
     axios.get("https://ucf.forums.rivals.com/forums/the-dungeon-knights-only.6/").then(function (response) {
       // Then, we load that into cheerio and save it to $ for a shorthand selector
       var $ = cheerio.load(response.data);
-      console.log(response.data)
+      // console.log(response.data);
       // Now, we grab every h2 within an article tag, and do the following:
-      $(".discussionListItems").each(function (i, element) {
+      $("h3.title").each(function (i, element) {
         // Save an empty result object
-        var result = {};
+        let title = $(element).text()
+        let link = $(element).children().attr("href")
+        console.log(title)
+        console.log(link)
 
-
-        // Add the text and href of every link, and save them as properties of the result object
-        result.title = $(this)
-          .children("a")
-          .text();
-        result.link = $(this)
-          .children("a")
-          .attr("href");
-
-        // Create a new Article using the `result` object built from scraping
-        db.Article.create(result)
-          .then(function (dbArticle) {
-            // View the added result in the console
-            console.log(dbArticle);
-          })
-          .catch(function (err) {
-            // If an error occurred, log it
-            console.log(err);
-          });
       });
 
       // Send a message to the client
       res.send("Scrape Complete");
     });
   });
-}
+};
