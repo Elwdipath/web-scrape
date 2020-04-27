a7362b3c41230d392e8cf1f275d345affb8d6791
Jordan Kaiser <jordan.kaiser@gmail.com>
2020-04-27 13:46:36 -0400

    added scrape route

--- server.js
+++ server.js
@@ -2,19 +2,36 @@ const express = require("express");
 const mongoose = require("mongoose");
 const axios = require("axios");
 const cheerio = require("cheerio");
-const logger = require("morgan");
-;
+// const logger = require("morgan");;
+const exphbs = require("express-handlebars")
+// const app = express()
 //models
 
 var db = require("./models");
 
 var app = express();
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+//middleware
+// app.use(logger("dev"));
 
-app.use(logger("dev"));
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
-// Make public a static folder
+
 app.use(express.static("public"));
+
+const MONGODB_URI =
+    process.env.MONGODB_URI || "mongodb://localhost/web-scrape";
+mongoose.connect(MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app)
+
+app.listen(PORT, function () {
+    console.log("Listening on PORT " + PORT);
+});
